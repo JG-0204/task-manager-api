34a869145f7309b25c6e124303213a3ff25a18a5
JG-0204 <131204758+JG-0204@users.noreply.github.com>
2024-07-27 02:11:16 +0800

    Add cors

--- index.js
+++ index.js
@@ -3,34 +3,45 @@ import http from 'http';
 import { urlMatcher } from './src/utils.js';
 
 import {
-    getTasksHandler,
-    getTaskHandler,
-    addTaskHandler,
-    updateTaskHandler,
-    deleteTaskHandler,
-    notFoundHandler,
+  getTasksHandler,
+  getTaskHandler,
+  addTaskHandler,
+  updateTaskHandler,
+  deleteTaskHandler,
+  notFoundHandler,
 } from './src/controllers/task.js';
+
+function handleCors(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+}
 
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
-    const { method, url } = req;
+const server = http.createServer(async (req, res) => {
+  handleCors(req, res);
 
-    if (method === 'GET' && url === '/api/tasks') {
-        getTasksHandler(req, res);
-    } else if (method === 'GET' && urlMatcher(url)) {
-        getTaskHandler(req, res);
-    } else if (method === 'POST' && url === '/api/tasks') {
-        addTaskHandler(req, res);
-    } else if (method === 'PUT' && urlMatcher(url)) {
-        updateTaskHandler(req, res);
-    } else if (method === 'DELETE' && urlMatcher(url)) {
-        deleteTaskHandler(req, res);
-    } else {
-        notFoundHandler(req, res);
-    }
+  const { method, url } = req;
+
+  if (method === 'GET' && url === '/api/tasks') {
+    getTasksHandler(req, res);
+  } else if (method === 'GET' && urlMatcher(url)) {
+    getTaskHandler(req, res);
+  } else if (method === 'POST' && url === '/api/tasks') {
+    addTaskHandler(req, res);
+  } else if (method === 'PUT' && urlMatcher(url)) {
+    updateTaskHandler(req, res);
+  } else if (method === 'DELETE' && urlMatcher(url)) {
+    deleteTaskHandler(req, res);
+  } else {
+    notFoundHandler(req, res);
+  }
 });
 
 server.listen(PORT, () => {
-    console.log(`Server running in port ${PORT}`);
+  console.log(`Server running in port ${PORT}`);
 });
