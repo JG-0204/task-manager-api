b4840021418d4dbe7130233ada9fc6e1c68563b5
JG-0204 <131204758+JG-0204@users.noreply.github.com>
2024-08-10 05:20:20 +0800

    Add new default props for task object

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -1,121 +1,151 @@
 import crypto from 'crypto';
 
-import { createRequestBody, doesExist } from '../utils.js';
+import { createRequestBody, doesExist, getCurrentDate } from '../utils.js';
 
 let tasks = [
-    {
-        id: crypto.randomUUID(),
-        task: 'Task 1',
-        description: 'Task 1 description',
-    },
-    {
-        id: crypto.randomUUID(),
-        task: 'Task 2',
-        description: 'Task 2 description',
-    },
-    {
-        id: crypto.randomUUID(),
-        task: 'Task 3',
-        description: 'Task 3 description',
-    },
-    {
-        id: crypto.randomUUID(),
-        task: 'Task 4',
-        description: 'Task 4 description',
-    },
-    {
-        id: crypto.randomUUID(),
-        task: 'Task 5',
-        description: 'Task 5 description',
-    },
-    {
-        id: crypto.randomUUID(),
-        task: 'Task 6',
-        description: 'Task 6 description',
-    },
+  {
+    id: crypto.randomUUID(),
+    task: 'Task 1',
+    description: 'Task 1 description',
+    priority: 'low',
+    dueDate: '8/16/24',
+    dateAdded: '8/10/24',
+    completed: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    task: 'Task 2',
+    description: 'Task 2 description',
+    priority: 'low',
+    dueDate: '8/16/24',
+    dateAdded: '8/10/24',
+    completed: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    task: 'Task 3',
+    description: 'Task 3 description',
+    priority: 'low',
+    dueDate: '8/16/24',
+    dateAdded: '8/10/24',
+    completed: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    task: 'Task 4',
+    description: 'Task 4 description',
+    priority: 'low',
+    dueDate: '8/16/24',
+    dateAdded: '8/10/24',
+    completed: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    task: 'Task 5',
+    description: 'Task 5 description',
+    priority: 'low',
+    dueDate: '8/16/24',
+    dateAdded: '8/10/24',
+    completed: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    task: 'Task 6',
+    description: 'Task 6 description',
+    priority: 'low',
+    dueDate: '8/16/24',
+    dateAdded: '8/10/24',
+    completed: false,
+  },
 ];
 
 export const getTasksHandler = (req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(JSON.stringify(tasks)),
-    });
-    res.end(JSON.stringify(tasks));
+  res.writeHead(200, {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(JSON.stringify(tasks)),
+  });
+  res.end(JSON.stringify(tasks));
 };
 
 export const getTaskHandler = (req, res) => {
-    const id = req.url.split('/')[3];
-    const task = tasks.find((task) => task.id === id);
-    if (!doesExist(tasks, id)) {
-        res.writeHead(404, {
-            'Content-Type': 'application/json',
-        });
-        res.write(JSON.stringify({ message: 'Task not found.' }));
-    } else {
-        res.writeHead(200, {
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(JSON.stringify(task)),
-        });
-        res.end(JSON.stringify(task));
-    }
+  const id = req.url.split('/')[3];
+  const task = tasks.find((task) => task.id === id);
+  if (!doesExist(tasks, id)) {
+    res.writeHead(404, {
+      'Content-Type': 'application/json',
+    });
+    res.end(JSON.stringify({ message: 'Task not found.' }));
+  } else {
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(JSON.stringify(task)),
+    });
+    res.end(JSON.stringify(task));
+  }
 };
 
 export const updateTaskHandler = (req, res) => {
-    const id = req.url.split('/')[3];
+  const id = req.url.split('/')[3];
 
-    if (!doesExist(tasks, id)) {
-        res.writeHead(404, {
-            'Content-Type': 'application/json',
-        });
-        res.write(JSON.stringify({ message: 'Task not found.' }));
-    } else {
-        createRequestBody(req, res, (body, res) => {
-            const currTask = tasks.find((task) => task.id === id);
-            const updatedTask = Object.assign(currTask, JSON.parse(body));
-            tasks = tasks.map((task) => (task.id !== id ? task : currTask));
+  if (!doesExist(tasks, id)) {
+    res.writeHead(404, {
+      'Content-Type': 'application/json',
+    });
+    res.write(JSON.stringify({ message: 'Task not found.' }));
+  } else {
+    createRequestBody(req, res, (body, res) => {
+      const currTask = tasks.find((task) => task.id === id);
+      const updatedTask = Object.assign(currTask, JSON.parse(body));
+      tasks = tasks.map((task) => (task.id !== id ? task : currTask));
 
-            res.writeHead(200, {
-                'Content-Type': 'application/json',
-                'Content-Length': Buffer.byteLength(
-                    JSON.stringify(updatedTask)
-                ),
-            });
-            res.write(JSON.stringify(updatedTask));
-        });
-    }
+      res.writeHead(200, {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(JSON.stringify(updatedTask)),
+      });
+      res.end(JSON.stringify(updatedTask));
+    });
+  }
 };
 
 export const deleteTaskHandler = (req, res) => {
-    const id = req.url.split('/')[3];
+  const id = req.url.split('/')[3];
 
-    if (!doesExist(tasks, id)) {
-        res.statusCode = 404;
-        res.write(JSON.stringify({ message: 'Task not found.' }));
-    } else {
-        const index = tasks.findIndex((task) => task.id === id);
+  if (!doesExist(tasks, id)) {
+    res.statusCode = 404;
+    res.end(JSON.stringify({ message: 'Task not found.' }));
+  } else {
+    const index = tasks.findIndex((task) => task.id === id);
 
-        tasks = tasks.toSpliced(index, 1);
-        res.statusCode = 204;
-        res.end();
-    }
+    tasks = tasks.toSpliced(index, 1);
+    res.statusCode = 204;
+    res.end(JSON.stringify({ message: 'Task successfully deleted.' }));
+  }
 };
 
 export const addTaskHandler = (req, res) => {
-    createRequestBody(req, res, (body, res) => {
-        const newTask = JSON.parse(body);
-        newTask.id = crypto.randomUUID();
-        tasks = tasks.concat(newTask);
-        res.writeHead(200, {
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(JSON.stringify(newTask)),
-        });
-        res.end(JSON.stringify(newTask));
+  createRequestBody(req, res, (body, res) => {
+    const newTask = JSON.parse(body);
+    newTask.id = crypto.randomUUID();
+    newTask.dateAdded = getCurrentDate();
+    newTask.completed = false;
+    tasks = tasks.concat(newTask);
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(JSON.stringify(newTask)),
     });
+    res.end(JSON.stringify(newTask));
+  });
 };
 
 export const notFoundHandler = (req, res) => {
-    res.writeHead(404, {
-        'Content-Type': 'application/json',
-    });
-    res.end(JSON.stringify({ message: 'Route not found' }));
+  try {
+    res.writeHead(404, 'Not found.');
+    res.write(
+      JSON.stringify({
+        message: 'Route not found.',
+      })
+    );
+  } catch (err) {
+    return;
+  }
 };
--- src/utils.js
+++ src/utils.js
@@ -1,25 +1,34 @@
 export const urlMatcher = (url) => {
-    return url.match(
-        /\/api\/tasks\/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}/
-    )
-        ? true
-        : false;
+  return url.match(
+    /\/api\/tasks\/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}/
+  )
+    ? true
+    : false;
 };
 
 export const doesExist = (taskArr, taskId) => {
-    return taskArr.find((task) => task.id === taskId);
+  return taskArr.find((task) => task.id === taskId);
 };
 
 export const createRequestBody = (req, res, callback) => {
-    let body = '';
+  let body = '';
 
-    req.on('error', (err) => {
-        console.error(err);
+  req
+    .on('error', (err) => {
+      console.error(err);
     })
-        .on('data', (chunk) => {
-            body += chunk;
-        })
-        .on('end', () => {
-            callback(body, res);
-        });
+    .on('data', (chunk) => {
+      body += chunk;
+    })
+    .on('end', () => {
+      callback(body, res);
+    });
+};
+
+export const getCurrentDate = () => {
+  const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+
+  const date = new Date();
+
+  return date.toLocaleDateString('en-US', options);
 };
